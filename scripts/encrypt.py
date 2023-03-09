4484af1257f40c68da586f551886e423e506c35f
h-ck3r <>
2023-03-09 23:32:38 +0000

    Adding site V2.

--- scripts/encrypt.py
+++ scripts/encrypt.py
@@ -7,15 +7,17 @@ password: str = argv[1] if len(argv) >= 2 else exit()
 
 os.system("git clone https://github.com/h-ck3r/website_source.git")
 
-for root, dirs, files in os.walk("./website_source"):
+for root, dirs, files in os.walk("./website_source/build"):
     for name in dirs:
         if "git" not in name:
             path: str = os.path.join(root, name)
-            os.system(f"mkdir -p {path.replace('website_source', 'docs')}")
+            os.system(f"mkdir -p {path.replace('website_source/build', 'docs')}")
         
     for name in files:
-        if name.split(".")[-1] == "html":
+        file_extension: str = name.split(".")[-1]
+
+        if file_extension == "html" or file_extension == "css":
             path: str = os.path.join(root, name)
-            os.system(f"npx pagecrypt {path} {path.replace('website_source', 'docs')} {password}")
+            os.system(f"npx pagecrypt {path} {path.replace('website_source/build', 'docs')} {password}")
 
 os.system("rm -rf website_source")
